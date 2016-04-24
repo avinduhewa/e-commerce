e9d4ef1c6be8603014e9a860a89c52254729fd6e
pankaja-gamage <pankaja.gamage96@gmail.com>
2016-04-24 19:46:25 +0530

    jdbc - add and delete needs to be modified when the functions are called! just a rough work so that we can easily edit and use

--- apps/backend/JDBC/src/Add.java
+++ apps/backend/JDBC/src/Add.java
@@ -1,9 +1,71 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class Add {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Add{
+	public static final String url = "jdbc:mysql://localhost/ecom";
+	public static final String username = "root";
+	public static final String password = "";
+	
+	public static void main(String[]args){
 
 	}
-
+	
+	// loading the data into an array list
+	public static void items(int itemid, String title, String description, String image, double price, String location, int sellerid, String status){
+		try{
+			String addItem = "INSERT INTO `item` (`itemid`, `title`, `description`, `image`, `price`, `creationdate`, `location`, `sellerid`, `status`) VALUES ('"+itemid+"', '"+title+"', '"+description+"', '"+image+"', '"+price+"', CURRENT_TIMESTAMP, '"+location+"', '"+sellerid+"', '"+status+"')";
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(addItem);
+			while (rslt.next()){
+				System.out.println();
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
+	
+	public static void sellers(int sellerid, int userid, String location){
+		try{
+			String addseller = "INSERT INTO `seller` (`sellerid`, `userid`, `location`) VALUES ('"+sellerid+"', '"+userid+"', '"+location+"')";
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(addseller);
+			while (rslt.next()){
+				System.out.println();
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
+	
+	public static void buyers(int buyerid, int userid){
+		try{
+			String addbuyer = "INSERT INTO `buyer` (`buyerid`, `userid`) VALUES ('"+buyerid+"', '"+userid+"')";
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(addbuyer);
+			while (rslt.next()){
+				System.out.println();
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
+	
+	public static void users(int userid, String fname, String lname, String email, String password, String image){
+		try{
+			String adduser = "INSERT INTO `user` (`userid`, `fname`, `lname`, `email`, `password`, `userimage`) VALUES ('"+userid+"', '"+fname+"', '"+lname+"', '"+email+"', '"+password+"', '"+image+"')";
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(adduser);
+			while (rslt.next()){
+				System.out.println();
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
 }
--- apps/backend/JDBC/src/Delete.java
+++ apps/backend/JDBC/src/Delete.java
@@ -1,9 +1,71 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class Delete {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Delete{
+	public static final String url = "jdbc:mysql://localhost/ecom";
+	public static final String username = "root";
+	public static final String password = "";
+	
+	public static void main(String[]args){
 
 	}
-
+	
+	// loading the data into an array list
+	public static void items(int itemid){
+		try{
+			String deleteitem = "DELETE FROM `item` WHERE `item`.`itemid` = "+itemid;
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(deleteitem);
+			while (rslt.next()){
+				System.out.println();
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
+	
+	public static void sellers(int sellerid){
+		try{
+			String deleteseller = "DELETE FROM `seller` WHERE `seller`.`sellerid` = "+sellerid;
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(deleteseller);
+			while (rslt.next()){
+				
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
+	
+	public static void buyers(int buyerid){
+		try{
+			String deltebuyer = "DELETE FROM `buyer` WHERE `buyer`.`buyerid` = "+buyerid;
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(deltebuyer);
+			while (rslt.next()){
+				
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
+	
+	public static void users(int userid){
+		try{
+			String deleteitem = "DELETE FROM `user` WHERE `user`.`userid` = "+userid;
+			Connection conn = DriverManager.getConnection(url,username,password);
+			Statement query = conn.createStatement();
+			ResultSet rslt = query.executeQuery(deleteitem);
+			while (rslt.next()){
+				System.out.println();
+			}
+		}catch(Exception ecx){
+			ecx.printStackTrace();
+		}
+	}
 }
